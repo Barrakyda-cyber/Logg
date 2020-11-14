4487a1e7c29f3194a2d176454e579ef0d539d637
Barrakyda-cyber <74377015+Barrakyda-cyber@users.noreply.github.com>
2020-11-15 02:34:46 +0300

    Add files via upload

---
+++ Logging/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LoggingLibrary;
+using static System.Console;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string text = $"{DateTime.Now}:{random.Next()}";
+            WriteLine(text);
+
+            Save save = new Save();
+            save.message += (m) => WriteLine(m);
+            save.Added(text);
+        }
+
+    }
+}
---
+++ LoggingLibrary/Save.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+
+namespace LoggingLibrary
+{
+    public delegate void Message(string m);
+    public class Save
+    {
+
+        public Message message;
+
+        public Save() { }
+
+        public Save(string path)
+        {
+            Path = path;
+        }
+        public string Path { get; private set; }
+
+        public async void Added(string message)
+        {
+            if (Path != null)
+            {
+                using (FileStream file = new FileStream($@"{Path}\log.txt", FileMode.Append))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes($"{message}\n");
+                    await file.WriteAsync(array, 0, array.Length);
+                }
+                this.message?.Invoke("Произошла запись в файл");
+            }
+            else
+            {
+                using (FileStream file = new FileStream("log.txt", FileMode.Append))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes($"{message}\n");
+                    await file.WriteAsync(array, 0, array.Length);
+                }
+                this.message?.Invoke("Произошла запись в файл, место файла стандартное");
+            }
+
+        }
+    }
+}
